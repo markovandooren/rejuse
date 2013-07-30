86d5d76f5f32bfa3ec2e18fa85a060dd45abee08
Marko van Dooren <Marko.van.Dooren@gmail.com>
2013-07-31 00:06:48 +0200

    more docs and more convenience methods

--- src/be/kuleuven/cs/distrinet/rejuse/action/Action.java
+++ src/be/kuleuven/cs/distrinet/rejuse/action/Action.java
@@ -1,5 +1,7 @@
 package be.kuleuven.cs.distrinet.rejuse.action;
 
+import java.util.Collection;
+
 /**
  * An class for actions.
  * 
@@ -17,7 +19,17 @@ public abstract class Action<T, E extends Exception> {
 	 * @param type A {@link Class} object that represents the type
 	 *             of objects on which the new action can operate.
 	 */
+ /*@
+   @ public behavior
+   @
+   @ pre type != null;
+   @
+   @ post type() == type;
+   @*/
 	public Action(Class<T> type) {
+		if(type == null) {
+			throw new IllegalArgumentException("The type of objects to which an action can be applied cannot be null.");
+		}
 		_type = type;
 	}
 	
@@ -27,7 +39,46 @@ public abstract class Action<T, E extends Exception> {
 	 * @param object The object on which the action must be performed.
 	 * @throws E
 	 */
-	public abstract void perform(T object) throws E;
+	protected abstract void doPerform(T object) throws E;
+	
+	/**
+	 * Perform the action on the given object. First,
+	 * the type of the object is checked. If it
+	 * is of type T, the action is applied. Otherwise,
+	 * nothing is done.
+	 * 
+	 * @param object The object to which the action should
+	 *               be applied.
+	 * @throws E
+	 */
+	public void perform(Object object) throws E {
+		if(type().isInstance(object)) {
+			doPerform((T) object);
+		}
+	}
+	
+	/**
+	 * Apply the action to the given collection. No
+	 * type check is performed. If you don't know
+	 * for sure that the collection contains only
+	 * objects of type T, use {@link #applySafely(Collection)}
+	 * instead.
+	 * 
+	 * @param collection The collection containing the objects to 
+	 * 								   which this action must be applied.
+	 * @throws E
+	 */
+	public void apply(Collection<T> collection) throws E {
+		for(T t: collection) {
+			doPerform(t);
+		}
+	}
+	
+	public void applySafely(Collection<?> collection) throws E {
+		for(Object o: collection) {
+			perform(o);
+		}
+	}
 	
 	/**
 	 * Return a class object that represents the type of
