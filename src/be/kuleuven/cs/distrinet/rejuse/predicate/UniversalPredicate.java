f83031d0891312fbff333bfef38f9324d65b9be3
Marko van Dooren <Marko.van.Dooren@gmail.com>
2013-08-11 21:32:51 +0200

    added and and guard

--- src/be/kuleuven/cs/distrinet/rejuse/predicate/UniversalPredicate.java
+++ src/be/kuleuven/cs/distrinet/rejuse/predicate/UniversalPredicate.java
@@ -4,12 +4,14 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import be.kuleuven.cs.distrinet.rejuse.action.Nothing;
 import be.kuleuven.cs.distrinet.rejuse.contract.Contracts;
 
 public abstract class UniversalPredicate<T, E extends Exception> extends AbstractPredicate<Object,E> {
 
-	public UniversalPredicate(Class<T> type) {
+	public UniversalPredicate(Class<? extends T> type) {
 		Contracts.notNull(type, "The class object of a universal predicate cannot be null.");
+		_type = type;
 	}
 	
 	@Override
@@ -20,11 +22,18 @@ public abstract class UniversalPredicate<T, E extends Exception> extends Abstrac
 		return false;
 	}
 	
+	public <O extends E> O evalAndCast(O object) throws E {
+		if(eval(object)) {
+			return (O) object;
+		}
+		return null;
+	}
+	
 	public abstract boolean uncheckedEval(T t) throws E;
 
-	private Class<T> _type;
+	private Class<? extends T> _type;
 	
-	public Class<T> type() {
+	public Class<? extends T> type() {
 		return _type;
 	}
 	
@@ -36,6 +45,31 @@ public abstract class UniversalPredicate<T, E extends Exception> extends Abstrac
   		}
   	}
   	return result;
+  }
+  
+  public UniversalPredicate<T,E> and(final UniversalPredicate<? super T, ? extends E> other) {
+  	return new UniversalPredicate<T, E>(type()) {
+			@Override
+			public boolean uncheckedEval(T object) throws E {
+				return UniversalPredicate.this.eval(object) && other.eval(object);
+			}
+		};
+  }
+  
+  @Override
+  public UniversalPredicate<T, Nothing> guard(final boolean value) {
+  	return new UniversalPredicate<T, Nothing>(type()) {
+
+			@Override
+			public boolean uncheckedEval(T object) throws Nothing {
+				try {
+					return UniversalPredicate.this.eval(object);
+				} catch(Exception e) {
+					return value;
+				}
+			}
+  		
+  	};
   }
 
 }
