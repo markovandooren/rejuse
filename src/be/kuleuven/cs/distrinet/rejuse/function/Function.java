1c82e15c35bd0372b60480c82fb04b9590b49a13
Marko van Dooren <Marko.van.Dooren@gmail.com>
2015-05-11 10:05:54 +0200

    adding documentation

--- src/be/kuleuven/cs/distrinet/rejuse/function/Function.java
+++ src/be/kuleuven/cs/distrinet/rejuse/function/Function.java
@@ -1,5 +1,14 @@
 package be.kuleuven.cs.distrinet.rejuse.function;
 
+/**
+ * An interface for functions that can throw checked exceptions.
+ * 
+ * @author Marko van Dooren
+ *
+ * @param <I> The type of the function parameter
+ * @param <O> The return type of the function.
+ * @param <E> The type of the checked exceptions that can be thrown.
+ */
 public interface Function<I,O,E extends Exception> {
 
 	
